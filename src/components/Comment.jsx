9a0a6408aba44c3603f173e86012a4b93fc156bf
caio <caioeliasvieira2@gmail.com>
2024-04-05 14:09:27 -0300

    created likes in comment component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,19 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
+
 import styles from './Comment.module.css'
+
 import { Avatar } from './Avatar'
+import { useState } from 'react';
 
 export function Comment({ content, onDeleteComment }) {
+    const [likeCount, setLikeCount] = useState(20);
+
     function handleDeleteComment() {
         onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount(likeCount + 1);
     }
 
     return (
@@ -31,9 +40,10 @@ export function Comment({ content, onDeleteComment }) {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir
+                        <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
